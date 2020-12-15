5e3d00a9ec4a987e12b52cbcf3f760d691594d03
Hüseyin Can Şimşek <51913434+HusoBey21@users.noreply.github.com>
2020-12-15 17:16:26 +0300

    Create yeni.m

---
+++ yeni.m
@@ -0,0 +1,12 @@
+a=imread('peppers.jpeg');
+>> b=rgb2gray(a);
+>> af=fft2(b);
+>> imshow(mat2gray(log(1+abs(af))));
+>> %%a secenegi cozumu
+
+
+>> c=imnoise(b,'gaussian',0.1);
+>> imshow(c);
+>> %% b secenegi cozumu
+>> %% a seceneginde fourier donusumu uygulandiginda goruntu tamamen bozuluyor b seceneginde gurultu
+eklendiginde netlik bozulsa da gurultu elde ediliyor
